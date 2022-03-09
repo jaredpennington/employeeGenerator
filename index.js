caefc234ce248afbf72fdaef525a05c0bc7a1c64
jaredpennington <94023319+jaredpennington@users.noreply.github.com>
2022-03-09 01:57:17 -0500

    updates

--- index.js
+++ index.js
@@ -12,6 +12,8 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
 var generateHTML = require("./src/generateHTML.js");
+const Manager = require('./lib/Manager');
+var teamArray = [];
 
 
 inquirer.prompt 
@@ -38,22 +40,119 @@ const questions = [
   },
   {
     type: "input",
-    name: "Office number?",
+    name: "officeNumber",
     message: "Manager's Office number?",
   },
 ];
 
+function start() {
+  function managerQs() {
+    inquirer.prompt(questions)
+    .then(res => {
+      //First we define new employee and passs arguments for each class data
+      
+      const manager = new Manager(res.Name, res.Id, res.Email, res.officeNumber)
+      // push the new employee into team array in order to pass it to generateHtml function
 
-inquirer.prompt(questions).then((answers) => {
-  for (let i = 0; i <  answers ; i++) {
-    if (answers.license === license[i]) answers.badges = badges[i];
+      teamArray.push(manager);
+      console.log(teamArray)
+      // Call cycle question to loop over the amount of interns and engineers the user would like to create until they select DONE option
+      
+      nextMember();
+    })
   }
 
-   writeToFile();
-});
+  function nextMember() {
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'choice',
+        message: 'What team member would you like to make next?',
+        choices: ['Engineer', 'Intern', 'Done']
+      }
+    ])
+    .then(res => {
+      switch(res.choice) {
+        case 'Engineer':
+          engineerQs();
+          break;
+        case 'Intern':
+          internQs();
+          break;
+        default:
+          writeToFile();
+          break;
+      }
+    })
+  }
 
-function writeToFile(fileName, data, extension) {
-    fs.writeFile(`${fileName}.${extension}`, generateHTML(data), (err) =>
+  function engineerQs() {
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'choice',
+        message: 'What team member would you like to make next?',
+        choices: ['Engineer', 'Intern', 'Done']
+      }
+    ])
+    .then(res => {
+      // push to team array
+      switch(res.choice) {
+        case 'Engineer':
+          engineerQs();
+          break;
+        case 'Intern':
+          internQs();
+          break;
+        default:
+          writeToFile();
+          break;
+      }
+      nextMember();
+    })
+
+  }
+
+  function internQs() {
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'choice',
+        message: 'What team member would you like to make next?',
+        choices: ['Engineer', 'Intern', 'Done']
+      }
+    ])
+    .then(res => {
+      // push to team array
+      switch(res.choice) {
+        case 'Engineer':
+          engineerQs();
+          break;
+        case 'Intern':
+          internQs();
+          break;
+        default:
+          writeToFile();
+          break;
+      }
+      nextMember();
+    })
+  }
+
+  managerQs();
+}
+start();
+
+// inquirer.prompt(questions).then((answers) => {
+//   for (let i = 0; i <  answers ; i++) {
+//     if (answers.license === license[i]) answers.badges = badges[i];
+//   }
+
+//    writeToFile();
+// });
+
+function writeToFile() {
+    fs.writeFile('index.html', generateHTML(teamArray), (err) =>
       err ? console.log(err) : console.log("Success!")
     );
   }
